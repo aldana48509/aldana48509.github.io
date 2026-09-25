44d2325162bf750c38e3bd6bae1ec0349eed85fc
Jose Aldana <aldana48509@students.apsb.org>
2026-09-25 12:45:33 +0000

    idk

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -30,7 +30,14 @@ var init = function (window) {
         view.addChild(circle);
         circles.push(circle);
     }
-            
+    Gamification.init({
+        canvas: canvas,
+        view: view,
+        draw: draw,
+        physikz: physikz,
+        circles: circles,
+        game: game
+    });
 
 
         // TODO 3 : Call the drawCircle() function
@@ -60,7 +67,7 @@ var init = function (window) {
             physikz.updatePosition(circles[i]);
             game.checkCirclePosition(circles[i]);
         }
-            
+        Gamification.update();    
         }
     
         /* 
